4bfcd6419b9334975ef64da04cf1929091ad6c00
kkaneko <kkaneko@student.42tokyo.jp>
2022-06-09 01:46:14 +0900

    norm error fixed

--- src/parser/free_texture.c
+++ src/parser/free_texture.c
@@ -3,21 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   free_texture.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
+/*   By: kkaneko <kkaneko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 17:39:07 by user42            #+#    #+#             */
-/*   Updated: 2022/06/08 18:37:07 by user42           ###   ########.fr       */
+/*   Updated: 2022/06/09 01:39:47 by kkaneko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void    free_texture(t_window *win, t_texture *tex)
+void	free_texture(t_window *win, t_texture *tex)
 {
-    if (tex == NULL
-        || win == NULL
-        || win->mlx == NULL)
-        return ;
-    mlx_destroy_image(win->mlx, tex->img->img);
-    free(tex);
+	if (tex == NULL
+		|| win == NULL
+		|| win->mlx == NULL)
+		return ;
+	mlx_destroy_image(win->mlx, tex->img->img);
+	free(tex);
 }
