083d039a2fca2ce9838b988efee4c07e794ff334
kkaneko <kkaneko@student.42tokyo.jp>
2022-06-08 21:07:53 +0900

    fix: norm error

--- src/parser/free_config.c
+++ src/parser/free_config.c
@@ -6,7 +6,7 @@
 /*   By: kkaneko <kkaneko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 17:26:50 by kkaneko           #+#    #+#             */
-/*   Updated: 2022/06/08 21:03:33 by kkaneko          ###   ########.fr       */
+/*   Updated: 2022/06/08 21:07:21 by kkaneko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ static void	free_paths(t_config *config);
 void	free_config(t_config *config)
 {
 	if (config == NULL)
-		return ;	
+		return ;
 	free_cub(config->cub);
 	free_paths(config);
 }
